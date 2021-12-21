dd6ecd5657aefb1b7f066bee883ea3332ba53e60
tharinee thuengnok <tharinee_t@cmu.ac.th>
2021-12-21 21:18:28 +0700

    react cookie consent

---
+++ my-app/src/App.js
@@ -0,0 +1,14 @@
+import CookieConsent from "./CookieConsentt";
+
+function App() {
+  return (
+    <div className="App">
+      Hello
+      <CookieConsent />
+      
+      
+    </div>
+  );
+}
+
+export default App;
